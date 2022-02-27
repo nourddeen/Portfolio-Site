66e0ceb7ecf6f85c015404177f6acbb170436b24
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-02-27 18:46:44 -0500

    set up all the different pages

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-function scrollto(div){
-    $('html,body').animate({
-    scrollTop: $("#"+div).offset().top
-    });
+
+const menuOpen = document.getElementById('menu-hamburger');
+$(menuOpen).click(function(e) {
+    $(".menu").toggleClass("menu-open");
+  });
+const menuClose = document.getElementById("home-menu")
+$(menuClose).click(function(e){
+    $(".menu").toggleClass("menu-open")
+})
+const menuSloseAbout = document.getElementById("about-menu")
+$(menuSloseAbout).click(function(){
+    $(".menu").toggleClass("menu-open")
+})
+const menuSloseResume = document.getElementById("resume-menu")
+$(menuSloseResume).click(function(){
+    $(".menu").toggleClass("menu-open")
+})
+const menuSloseProjects = document.getElementById("projects-menu")
+$(menuSloseProjects).click(function(){
+    $(".menu").toggleClass("menu-open")
+})
+function scrollto(div)
+{
+ $('html,body').animate(
+ {
+  scrollTop: $("#"+div).offset().top
+ },'slow');
 }
